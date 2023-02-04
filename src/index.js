adc8f134c159e2c877f3d26e531671b15483de66
Chance Gibbs <kcg005@shsu.edu>
2023-02-03 22:19:13 -0600

    tempo set for sixteenth notes, audio files now properly choke instead of waiting for playback to end before retriggering

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import BD from './assets/BD.wav';
 import SD from './assets/SD.wav';
 import HH from './assets/HH.wav';
+import {Howl, Howler} from 'howler';
 
 function Step(props) {
   return (
@@ -12,41 +13,6 @@ function Step(props) {
     </button>
   )
 }
-
-// class Board extends React.Component {
-//   renderSquare(i) {
-//     return (
-//       <Square 
-//         value={this.props.squares[i]} 
-//         onClick={() => this.props.onClick(i)}
-//       />
-//     );
-//   }
-
-//   render() {
-//     const kickSteps = this.props.sequencers.kickSeq.map()
-//     return (
-//       <div>
-//         <div className="status">{this.props.status}</div>
-//         <div className="board-row">
-//           {this.renderSquare(0)}
-//           {this.renderSquare(1)}
-//           {this.renderSquare(2)}
-//         </div>
-//         <div className="board-row">
-//           {this.renderSquare(3)}
-//           {this.renderSquare(4)}
-//           {this.renderSquare(5)}
-//         </div>
-//         <div className="board-row">
-//           {this.renderSquare(6)}
-//           {this.renderSquare(7)}
-//           {this.renderSquare(8)}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 
 class SequencerRow extends React.Component {
   constructor(props) {
@@ -95,7 +61,7 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      bpm: 130,
+      bpm: 170,
       playing: false,
       sequencers: {
         kickSeq: Array(16).fill(0),
@@ -105,34 +71,19 @@ class Game extends React.Component {
       stepNumber: 0,
     }
 
-    this.kickSound = new Audio(BD);
-    this.snareSound = new Audio(SD);
-    this.hatSound = new Audio(HH);
+    // this.kickSound = new Audio(BD);
+    // this.snareSound = new Audio(SD);
+    // this.hatSound = new Audio(HH);
+    this.kickSound = new Howl({
+      src: BD,
+    })
+    this.snareSound = new Howl({
+      src: SD,
+    })
+    this.hatSound = new Howl({
+      src: HH,
+    })
   }
-
-  // handleClick(i) {
-  //   const history = this.state.history.slice(0, this.state.stepNumber + 1);
-  //   const current = history[this.state.stepNumber];
-  //   const squares = current.squares.slice();
-  //   if (calculateWinner(squares) || squares[i]) {
-  //     return;
-  //   }
-  //   squares[i] = this.state.xIsNext ? 'X' : 'O';
-  //   this.setState({
-  //     history: history.concat([{
-  //       squares: squares,
-  //     }]),
-  //     stepNumber: history.length,
-  //     xIsNext: !this.state.xIsNext,
-  //   });
-  // }
-
-  // jumpTo(step) {
-  //   this.setState({
-  //     stepNumber: step,
-  //     xIsNext: (step % 2) === 0,
-  //   });
-  // }
 
   startStop = () => {
     if (this.state.playing) {
@@ -143,9 +94,8 @@ class Game extends React.Component {
       clearInterval(this.timer);
       return;
     }
-    this.timer = setInterval(() => this.playSounds(), (30 / this.state.bpm) * 1000);
+    this.timer = setInterval(() => this.playSounds(), (15 / this.state.bpm) * 1000);
     this.setState({
-      //sequencers: this.state.sequencers,
       playing: true,
     })
   }
@@ -165,7 +115,6 @@ class Game extends React.Component {
     }
 
     this.setState({
-      //sequencers: this.state.sequencers,
       stepNumber: (this.state.stepNumber + 1) % 16,
     })
     console.log(this.state);
@@ -195,8 +144,6 @@ class Game extends React.Component {
           </span>
         </div>
         <div className="game-info">
-          {/* <div>{status}</div> */}
-          {/* <ol>{moves}</ol> */}
         </div>
         <button onClick={this.startStop}>
           Start/Stop
@@ -205,26 +152,6 @@ class Game extends React.Component {
     );
   }
 }
-
-// function calculateWinner(squares) {
-//   const lines = [
-//     [0, 1, 2],
-//     [3, 4, 5],
-//     [6, 7, 8],
-//     [0, 3, 6],
-//     [1, 4, 7],
-//     [2, 5, 8],
-//     [0, 4, 8],
-//     [2, 4, 6],
-//   ];
-//   for (let i = 0; i < lines.length; i++) {
-//     const [a, b, c] = lines[i];
-//     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-//       return squares[a];
-//     }
-//   }
-//   return null;
-// }
 
 // ========================================
 
